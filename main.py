3be432cf3867bfb7fd4465fefd9fc04327c6319e
John Dang <john.amq.dang@gmail.com>
2020-03-16 10:34:16 -0700

    added num workers to main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from policy_gradient import *
 import os
 
-def main(envs, hiddens, train_iters=50, eval_iters=5):
+def main(envs, hiddens, train_iters=50, eval_iters=5, num_workers=1):
     # directory for storing models and performancce data
     if not os.path.exists("./models"):
         os.mkdir("./models")
@@ -31,22 +31,22 @@ def main(envs, hiddens, train_iters=50, eval_iters=5):
             print("\nUsing hidden dims ", hidden_dims)
 
             print("\nRunning MAML")
-            maml_pg(env_name=env, num_iterations=train_iters, policy_hidden=hidden_dims)
+            maml_pg(env_name=env, num_iterations=train_iters, policy_hidden=hidden_dims, num_workers=num_workers)
 
             print("\nRunning Pretrain")
-            pretrain_pg(env_name=env, num_iterations=train_iters, policy_hidden=hidden_dims)
+            pretrain_pg(env_name=env, num_iterations=train_iters, policy_hidden=hidden_dims, num_workers=num_workers)
             
             # evaluate scratch vs pretrain vs maml
             print("\nTraining from scratch")
-            train_pg(env_name=env, num_iterations=eval_iters, policy_hidden=hidden_dims, mode_str="scratch")
+            train_pg(env_name=env, num_iterations=eval_iters, policy_hidden=hidden_dims, mode_str="scratch", num_workers=num_workers)
 
             print("\nTraining from MAML")
             maml_path = './models/' + env + '/' + "_".join([str(n) for n in hidden_dims]) + '/pg_maml' + '.pt'
-            train_pg(env_name=env, num_iterations=eval_iters, policy_hidden=hidden_dims, filepath=maml_path, mode_str="maml")
+            train_pg(env_name=env, num_iterations=eval_iters, policy_hidden=hidden_dims, filepath=maml_path, mode_str="maml", num_workers=num_workers)
 
             print("\nTraining from Pretrain")
             pretrain_path = './models/' + env + '/' + "_".join([str(n) for n in hidden_dims]) + '/pg_pretrain' + '.pt'
-            train_pg(env_name=env, num_iterations=eval_iters, policy_hidden=hidden_dims, filepath=pretrain_path,mode_str="pretrain")
+            train_pg(env_name=env, num_iterations=eval_iters, policy_hidden=hidden_dims, filepath=pretrain_path,mode_str="pretrain", num_workers=num_workers)
 
 
 
@@ -73,4 +73,4 @@ if __name__ == '__main__':
         [128,128,128]
     ]
 
-    main(envs, hiddens, train_iters=50, eval_iters=10)
+    main(envs, hiddens, train_iters=50, eval_iters=10, num_workers=1)
